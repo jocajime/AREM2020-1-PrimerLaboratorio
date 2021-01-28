5bdf3e72382281e9c3c16e9ac1bd06b6865554dc
joel carvajal <joel.carvajal@mail.escuelaing.edu.co>
2021-01-28 18:37:08 -0500

    segunda version del proyecto adicionando e ignorando mas archivos

---
+++ src/main/java/edu/escuelaing/arem/ASE/app/App.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arem.ASE.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
